12b0447af7fb368f6c1fb50bc3dae83c7157a64e
maiaratobias-ship-it <maiara.tobias@escola.pr.gov.br>
2026-09-21 08:11:16 -0300

    25

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = documento.queryselector (". caixa-principal");
+const caixaPerguntas = documento.queryselector (". caixa-perguntas");
+const caixaAlternativas = documento.queryselector (". caixa-alternativas");
+const caixaResultados = documento.queryselector (". caixa-rersultados");
+const tetxoresultados = documento.queryselector (". texto-resultados");
+
+const pergunta = [
+    {
+        enunciado:"pergunta 1",
+        alternativa:[
+            "altrnativa 1",
+            "alyernativa 2"
+        ]
+    },
+    {
+     enunciado:"pergunta 1",
+        alternativa:[
+            "altrnativa 1",
+            "alyernativa 2"
+        ]   
+    }
+];
